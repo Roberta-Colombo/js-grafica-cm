1c459a568c4950bed3db96571d94591fc1d08860
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-10-17 17:42:25 +0200

    Aggiunto bonus livelli di difficoltà

--- js/script.js
+++ js/script.js
@@ -14,11 +14,32 @@ const playBtn = document.getElementById('play-btn');
 function play(){
     console.log("Start");
     const playfield = document.querySelector('.playfield');
+    playfield.innerHTML = '';
+    const level = document.getElementById('level').value;
+    // console.log(level);
+
+    let numOfSquares;
+    switch(level){
+        case '1':
+        default: 
+            numOfSquares = 100;
+        break;  
+        case '2':
+            numOfSquares = 81;
+        break;
+        case '3':
+            numOfSquares = 49;
+        break;           
+    }
+
     
     let num;
     function createSquare(num){
+        let squaresPerSide = Math.sqrt(numOfSquares);
         const square = document.createElement('div');
         square.className = 'square';
+        square.style.width = ` calc(100% / ${squaresPerSide}) `;
+        square.style.height = ` calc(100% / ${squaresPerSide}) `;
         square.innerHTML = `
         <span>${num}</span>
         `;
@@ -31,13 +52,12 @@ function play(){
         return square; 
     }
     createSquare(num);
-
     
 
     function createGrid(){
         const grid = document.createElement('div');
         grid.className = 'grid';
-        for(let i = 1; i <= 100; i++){
+        for(let i = 1; i <= numOfSquares; i++){
             const square = createSquare(i);
             grid.appendChild(square);
         }
