158ef74766b5f921c5099700b302009bd8f28ce6
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-10-17 16:27:04 +0200

    creata griglia con 100 celle

--- js/script.js
+++ js/script.js
@@ -8,3 +8,35 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe; */
+
+const playBtn = document.getElementById('play-btn');
+
+function play(){
+    console.log("Start");
+    const playfield = document.querySelector('.playfield');
+    
+    let num;
+    function createSquare(num){
+        const square = document.createElement('div');
+        square.className = 'square';
+        square.innerHTML = `
+        <span>${num}</span>
+        `;
+        return square;
+    }
+    createSquare(num);
+
+    function createGrid(){
+        const grid = document.createElement('div');
+        grid.className = 'grid';
+        for(let i = 1; i <= 100; i++){
+            const square = createSquare(i);
+            grid.appendChild(square);
+        }
+        playfield.appendChild(grid);
+    }   
+    createGrid();
+}
+
+
+playBtn.addEventListener('click', play);
